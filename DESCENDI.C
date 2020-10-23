35d1e34d3bd88c83df9c9e7712a07f38a31ce1fc
Shubham Narayan More <47040666+ShubhamX12@users.noreply.github.com>
2020-10-23 10:40:32 -0700

    using this c program to arrange element in descending order

---
+++ DESCENDI.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[5]={11,1,22,3,4};
+  int sm,i,j,temp;
+  clrscr();
+  for(i=0;i<=3;i++)
+  {
+   for(j=0;j<=3-i;j++)
+   {
+	if(a[j]<a[j+1])
+	{
+		temp=a[j];
+		a[j]=a[j+1];
+		a[j+1]=temp;
+
+	}
+
+   }
+
+  }
+  for(i=0;i<5;i++)
+  {
+    printf("\n%d",a[i]);
+   }
+ getch();
+}
